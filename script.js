54abe3116ef16917500a73c92aa81e6a8a41a987
peu1311 <93681704+Peu1311@users.noreply.github.com>
2021-11-23 20:02:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function mouFocus(a){
+    a.classList.add('focus');
+}
+
+function mouBlur(a){
+    a.classList.remove('focus');
+}
+
+function navOver(a){
+    a.classList.add('navOver');
+}
+
+function navOut(a){
+    a.classList.remove('navOver');
+}
+
+function footOver(a){
+    a.classList.add('footOver');
+}
+
+function footOut(a){
+    a.classList.remove('footOver');
+}
+
+function linkOver(a){
+    a.classList.add('linkHover');
+}
+
+function linkOut(a){
+    a.classList.remove('linkHover');
+}
+
+function linkClick(a){
+    a.classList.add('linkClick');
+}
