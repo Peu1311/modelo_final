24d2548966aab6c894ad37e81a6fffb0e33bebe1
peu1311 <93681704+Peu1311@users.noreply.github.com>
2022-01-18 15:18:56 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-if(innerWidth <= 600){
-    window.document.querySelector('.default').classList.add('phone');
-}
-else if(innerWidth <= 992){
-    window.document.querySelector('.default').classList.add('note');
-}
-else{
-    window.document.querySelector('.default').classList.add('desktop');
-}
