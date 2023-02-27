e43678117e6b59082966e57f1c558aac35367825
peu1311 <93681704+Peu1311@users.noreply.github.com>
2023-02-27 20:07:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 window.document.querySelector('button').addEventListener('click',()=>{
+    window.document.querySelector('main').innerHTML=''
+
     var opt={
         method:'GET',
         mode:'cors',
@@ -6,5 +8,9 @@ window.document.querySelector('button').addEventListener('click',()=>{
     }
     fetch('./objeto.json',opt)
     .then(data=>data.json())
-    .then(dados=>console.log(dados))
+    .then(dados=>{
+        var img = window.document.createElement('img')
+        window.document.querySelector('main').appendChild(img)
+        window.document.querySelector('img').src=dados.pizza
+    })
 })
